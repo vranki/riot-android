95337d02af7f6378eed9fd6574193fc3f02a36cf
Valere <valeref@matrix.org>
2018-12-13 16:04:00 +0100

    Fix after rebase
    VectorBaseFragment need to implement layout resid
    Fix bad play services text copy
    FCMHelper should not show dialog to fix playservices

--- vector/src/main/java/im/vector/fragments/VectorSettingsNotificationsTroubleshootFragment.kt
+++ vector/src/main/java/im/vector/fragments/VectorSettingsNotificationsTroubleshootFragment.kt
@@ -43,8 +43,12 @@ class VectorSettingsNotificationsTroubleshootFragment : VectorBaseFragment() {
 
     var testManager: NotificationTroubleshootTestManager? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_settings_notifications_troubleshoot, container, false)
+    override fun getLayoutResId() = R.layout.fragment_settings_notifications_troubleshoot
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         mRecyclerView = view.findViewById(R.id.troubleshoot_test_recycler_view);
         val layoutManager = LinearLayoutManager(context)
@@ -69,8 +73,6 @@ class VectorSettingsNotificationsTroubleshootFragment : VectorBaseFragment() {
         }
 
         startUI()
-
-        return view
     }
 
     private fun startUI() {
